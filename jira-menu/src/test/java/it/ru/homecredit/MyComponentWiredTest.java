6f6a1772274d5944ab343f7786b42f5691840236
ignat <ignat.nushtaev@outlook.com>
2022-07-28 19:47:00 +0300

    initial

--- jira-menu/src/test/java/it/ru/homecredit/MyComponentWiredTest.java
+++ jira-menu/src/test/java/it/ru/homecredit/MyComponentWiredTest.java
@@ -1,10 +1,10 @@
 package it.ru.homecredit;
 
+import com.atlassian.plugins.osgi.test.AtlassianPluginsTestRunner;
+import com.atlassian.sal.api.ApplicationProperties;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import com.atlassian.plugins.osgi.test.AtlassianPluginsTestRunner;
 import ru.homecredit.api.MyPluginComponent;
-import com.atlassian.sal.api.ApplicationProperties;
 
 import static org.junit.Assert.assertEquals;
 
